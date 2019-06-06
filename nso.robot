79bb0b9442873930826ff199a038d4cdb241a265
Jason Belk <jason.belk@networktocode.com>
2019-06-06 10:10:39 -0700

    fixed nso robot

--- nso.robot
+++ nso.robot
@@ -1,31 +1,32 @@
 *** Settings ***
 Library    Collections
-Library    utils/nso_lib.py
+Library    utils/test_nso.py
 
 ** Variables **
 ${nso_devicename}       csr1
 ${command}      show version
 ${ios_version}      16.06.02
 ${acl_ip_to_find}       91.118.192.128
-
+${device_type}      ios-xe
 *** Test Cases ***
 
 
 
 Check Sync NSO against a Live Device: Initial check
-    ${return}=    Cdb Synced With Device   ${nso_devicename}
+    ${return}=    Check Sync   ${nso_devicename}
     Should Be True    ${return}
 
-Verify IOS Version is Correct
-    ${return}=    Send Cli Cmd   ${nso_devicename}  ${command} 
-    Should Contain    ${return}     ${ios_version} 
-Verify IP is in ACL
-    @{return}=    Find IP Access List   ${acl_ip_to_find}
-    Log List  ${return}
-    :FOR  ${acl_iter}  IN  @{return}
-    \    ${rule}=  Get From Dictionary  ${acl_iter}  rule
-    \    Log  ${rule}
-    # \    # Option 2
-    # \    Log  ${acl_iter["rule"]}
-    # \    Should Contain  ${acl_iter["rule"]}  ${acl_ip_to_find}
-    END
+# Verify IOS Version is Correct
+#     ${return}=    Send Cli Cmd   ${nso_devicename}  ${command} 
+#     Should Contain    ${return}     ${ios_version} 
+# Verify IP is in ACL
+#     @{return}=    Find IP Access List   ${acl_ip_to_find} ${device_type}
+#     Log List  ${return}
+#     :FOR  ${acl_iter}  IN  @{return}
+#     \    ${rule}=  Get From Dictionary  ${acl_iter}  rule
+#     \    Log  ${rule}
+#     # \    # Option 2
+#     # \    Log  ${acl_iter["rule"]}
+#     # \    Should Contain  ${acl_iter["rule"]}  ${acl_ip_to_find}
+#     END
+
---
+++ utils/test_nso.py
@@ -0,0 +1,62 @@
+import ncs
+
+def check_sync(nso_devicename):
+    with ncs.maapi.single_read_trans('admin', 'python', groups=['ncsadmin']) as t:
+        root = ncs.maagic.get_root(t)
+        output = root.devices.device[nso_devicename].check_sync()
+        if output.result.string == "in-sync":
+            return True
+        elif output.result.string == "out-of-sync":
+            return False
+        else:
+            return False
+
+def send_cli_cmd(nso_devicename,command=None):
+    """
+    Sends a CLI command directly to a device and returns its output as a new line separated list.
+    """
+    try:
+        if command:
+            with ncs.maapi.single_read_trans('admin', 'python', groups=['ncsadmin']) as t:
+                    root = ncs.maagic.get_root(t)
+                    device = root.devices.device[nso_devicename]
+                    input1 = device.live_status.ios_stats__exec.show.get_input()
+                    input1.args = [command]
+                    output = device.live_status.ios_stats__exec.any(input1).result
+                    return output
+    except:
+        return False
+
+
+def find_ip_access_list(ip):
+    """
+    Single search to see if a provided IP address
+    is present inside any of a devices extended ACLs.
+    """
+    acl_answer = []
+    with ncs.maapi.single_read_trans('admin', 'python', groups=['ncsadmin']) as t:
+        root = ncs.maagic.get_root(t)
+        for box in root.devices.device:
+            print("checking Standard Access Lists for " + str(box.name))
+            for acl in root.devices.device[box.name].config.ip.access_list.standard.std_named_acl:
+                print("checking ip access-list standard " + str(acl.name))
+                for rule in root.devices.device[box.name].config.ip.access_list.standard.std_named_acl[acl.name].std_access_list_rule:
+                    if ip in rule.rule:
+                        print(ip + " Is in acl " + str(acl.name))
+                        acl_answer.append({"name":str(acl.name),"rule":rule.rule})
+            print("checking Extended Access Lists for " + str(box.name))
+            for acl in root.devices.device[box.name].config.ip.access_list.extended.ext_named_acl:
+                print("checking ip access-list extended " + str(acl.name))
+                for rule in root.devices.device[box.name].config.ip.access_list.extended.ext_named_acl[acl.name].ext_access_list_rule:
+                    if ip in rule.rule:
+                        print(ip + " Is in acl " + str(acl.name))
+                        acl_answer.append({"name":str(acl.name),"rule":rule.rule})
+    return acl_answer 
+
+# with ncs.maapi.single_read_trans('admin', 'python', groups=['ncsadmin']) as t:
+#         root = ncs.maagic.get_root(t)
+#         device = root.devices.device[nso_devicename]
+#         input1 = device.live_status.ios_stats__exec.show.get_input()
+#         input1.args = [command]
+#         output = device.live_status.ios_stats__exec.any(input1).result
+#         print( output)
